77c81301c289f9c04bd59416405e6d0e91cfe722
Goodness Akalazu <goodnessakalazu@immersely.ai>
2024-01-28 18:44:38 +0000

    modified:   src/components/features/features.js

--- src/components/features/features.js
+++ src/components/features/features.js
@@ -16,8 +16,8 @@ function Features() {
 
     // Initialize state for storing the data
     const [publishers, setPublishers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [ setLoading] = useState(true);
+    const [ setError] = useState(null);
     // Create a single supabase client for interacting with your database
     const supabase = createClient('https://kcfgjenxuummtzdtprsh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjZmdqZW54dXVtbXR6ZHRwcnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5Nzg3NjMsImV4cCI6MjAxNzU1NDc2M30.F8A-w41SZeMzbkWTYl2ROoSqB05b-nHPwa-tAKl_PJY')
 
